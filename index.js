01d86ac25f100e9a88d35245eb6f1820fd6fac6c
MICHELLE NASIRISI <wanjikunasirisi1@gmail.com>
2023-12-16 02:28:01 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+// Function expression divide
+function divide() {
+    return 2000 / 100;
+  }
+  
+  // Arrow function square
+  const square = (x) => x * x;
+  
+  // Arrow function add
+  const add = (a, b) => a + b;
+  
+  module.exports = { divide, square, add };
+  
